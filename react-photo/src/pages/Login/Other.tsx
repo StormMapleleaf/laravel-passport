b616c836717fd04833c40d62bfb0094a97b6aa75
dai_qi_xiang <1290023174@qq.com>
2024-08-16 11:00:56 +0800

    docker

--- react-photo/src/pages/Login/Other.tsx
+++ react-photo/src/pages/Login/Other.tsx
@@ -3,8 +3,8 @@ import { Button } from 'antd';
 
 const AuthButton = () => {
     const handleAuth = () => {
-        const clientId = '48421d47-8e20-4cbb-a487-24bdcfb2081d';
-        const redirectUri = 'http://localhost:3000/home';
+        const clientId = '9cc54706-9344-4f1f-ab55-eb4fc0771fa8';
+        const redirectUri = 'http://localhost:8000/welcome';
         const responseType = 'code';
         const scope = '*';
         const authUrl = `http://localhost:8080/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=${responseType}&scope=${scope}`;
